80ce442b7d238330e7b3916b84287bbab706c4d8
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2024-02-26 23:03:54 +0100

    Agregar archivos de proyecto.

---
+++ PiedraPapelTijera_BKLP/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PiedraPapelTijera_BKLP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly List<string> opcionesValidas = new List<string> { "piedra", "papel", "tijera" };
+
+        [HttpPost]
+        public ActionResult<string> Play(string player1, string player2)
+        {
+            if (!opcionesValidas.Contains(player1) || !opcionesValidas.Contains(player2))
+                return "Al menos una de las opciones ingresadas no es válida.";
+
+            if (player1 == player2)
+                return "Empate";
+
+            if ((player1 == "piedra" && player2 == "tijera") ||
+                (player1 == "papel" && player2 == "piedra") ||
+                (player1 == "tijera" && player2 == "papel"))
+                return "Gana jugador 1";
+
+            return "Gana jugador 2";
+        }
+    }
+}
